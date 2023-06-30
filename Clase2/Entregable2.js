2fa8bc8f260606ff437bfa66472041276457769b
pazgraziano <mariapazgraziano@gmail.com>
2023-06-30 15:16:38 -0300

    add: Entregable2.js

---
+++ Clase2/Entregable2.js
@@ -0,0 +1,13 @@
+const lista = ["Paz Graziano", 39, true];
+console.log(lista)
+
+const fecha_nacimiento = new Date("december 28 1983");
+console.log(fecha_nacimiento)
+
+const libro = {
+    titulo: "La muerte del comendador",
+    autor: "Haruki Murakami",
+    anyo: 2017,
+    url: "https://www.buscalibre.com.ar/libro-la-muerte-del-comendador-libros-1-y-2/9788490667590/p/52154702?bmkt_source=google&bmkt_campaign=601424760&gclid=CjwKCAjw-vmkBhBMEiwAlrMeF4pCLwJBTkv4Ag86MeNoOJOEPNc9ORrVpXsEgUFSwbm0m5bhOWGWGBoCn14QAvD_BwE"
+}
+console.log(libro)
